b52817be003d84c243f3c2169ff6c5bb0a881f41
AlexMamani <alexander_mamani_22@hotmail.com>
2019-05-20 13:40:28 -0300

    Se agrego una clase

---
+++ src/main/java/aplicacion/controlador/beans/forms/JujuyFormBeans.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplicacion.controlador.beans.forms;
+
+import javax.inject.Named;
+import javax.faces.view.ViewScoped;
+
+/**
+ *
+ * @author alexa
+ */
+@ManagedBeans
+@ViewScoped
+public class JujuyFormBeans {
+
+    /**
+     * Creates a new instance of JujuyFormBeans
+     */
+    public JujuyFormBeans() {
+    }
+    
+}
